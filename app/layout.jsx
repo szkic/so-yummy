471d1da2afb9c13b80f7e57d9a23ab2cbe360a00
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-11-02 13:49:17 +0100

    added welcome logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import "@styles/globals.css";
 import { poppins } from "@styles/fonts";
+import Header from "@components/Header";
 
 export const metadata = {
   title: "So Yummy",
-  description: "coś tu ładnie opisać",
+  description: "Cooking recipes app",
 };
 
 const RootLayout = ({ children }) => {
+  const isLoggedIn = false;
+
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
         <div className="main">
+          {isLoggedIn && (
+            <header>
+              <Header />
+            </header>
+          )}
           <main>{children}</main>
         </div>
       </body>
--- components/AuthNav.jsx
+++ components/AuthNav.jsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const AuthNav = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-11 bg-[url('/assets/images/welcome-mobile.png')] bg-cover bg-no-repeat text-primary-text-color tablet:gap-10 tablet:bg-[url('/assets/images/welcome-tablet.png')] desktop:bg-[url('/assets/images/welcome-desktop.png')]">
+      <div className="flex h-[54px] w-[54px] justify-center rounded-xl bg-primary-color pl-[0.5px] pt-1 tablet:hidden">
+        <Image
+          src="/../assets/images/logo.svg"
+          height={44}
+          width={44}
+          alt="logo"
+        />
+      </div>
+
+      <div className="hidden h-[68px] w-[68px] justify-center rounded-xl bg-primary-color pl-[0.7px] pt-1 tablet:flex">
+        <Image
+          src="/../assets/images/logo.svg"
+          height={44}
+          width={44}
+          alt="logo"
+        />
+      </div>
       <div className="w-[305px] text-center tablet:w-[505px] desktop:w-[540px]">
         <h2 className="mb-3.5 text-2xl leading-6 tracking-[-0.48px] tablet:text-[28px] tablet:leading-7 tablet:tracking-[-0.56px]">
           Welcome to the app!
