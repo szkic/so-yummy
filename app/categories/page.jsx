f290982012257d376f61f0c4b3f4e939075c88e3
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-01-21 13:47:05 +0100

    added categories tablet & desktop styling

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -22,9 +22,12 @@ const CategoriesPage = () => {
     <>
       <section>
         <h2 className="text-3xl font-semibold">Categories</h2>
-        <ul className="mt-12 flex gap-7 overflow-auto border-b-2 pb-5">
+        <ul className="mt-12 flex gap-7 overflow-auto border-b-2 pb-5 tablet:gap-14 desktop:mt-24">
           {categories.map((category) => (
-            <li key={category} className="text-inactive-text-light">
+            <li
+              key={category}
+              className="text-inactive-text-light px-5 text-sm tablet:text-lg tablet:text-inactive-text"
+            >
               <Link href={`/categories/${category}`}>{category}</Link>
             </li>
           ))}
