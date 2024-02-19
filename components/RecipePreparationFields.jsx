cfebd4515480ae15524eb7ad8bbfd259e29babf7
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-02-19 17:57:06 +0100

    added Recipe Preparation

--- components/RecipePreparationFields.jsx
+++ components/RecipePreparationFields.jsx
@@ -1,7 +1,20 @@
+import { TextField } from "@mui/material";
 import React from "react";
 
 const RecipePreparationFields = () => {
-  return <div>RecipePreparationFields</div>;
+  return (
+    <div className="mt-11">
+      <h3 className="text-2xl font-semibold">Recipe Preparation</h3>
+      <TextField
+        id="recipe"
+        label="Enter recipe"
+        multiline
+        rows={5}
+        fullWidth
+        className="mt-6"
+      />
+    </div>
+  );
 };
 
 export default RecipePreparationFields;
