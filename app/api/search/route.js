e86204e4985eddc940512d5a52686b55bf57d63d
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-02-24 12:19:53 +0100

    find recipe by ingredient

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,23 +1,46 @@
+import Ingredient from "@models/ingredient";
+
 const { default: Recipe } = require("@models/recipe");
 const { connectToDB } = require("@utils/database");
 
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
-  const title = searchParams.get("title");
-  const ingredient = searchParams.get("ingredient");
+  const urlParam = (param) => searchParams.get(param);
 
   try {
     await connectToDB();
 
     if (searchParams.has("title")) {
       const recipesByTitle = await Recipe.find({
-        title: { $regex: new RegExp(title, "i") },
+        title: { $regex: new RegExp(urlParam("title"), "i") },
       });
 
       return new Response(JSON.stringify(recipesByTitle), { status: 200 });
     }
 
     if (searchParams.has("ingredient")) {
+      const searchIngredient = await Ingredient.find({
+        ttl: { $regex: new RegExp(urlParam("ingredient"), "i") },
+      });
+
+      // search for ingredient IDs
+      const targetIngredientId = searchIngredient.map((id) =>
+        id._id.toString(),
+      );
+
+      // return all Recipes with ingredients searched by IDs
+      const recipesWithIngredient = await Promise.all(
+        targetIngredientId.map(
+          async (id) =>
+            await Recipe.find({
+              "ingredients.id": id,
+            }),
+        ),
+      );
+
+      return new Response(JSON.stringify(recipesWithIngredient), {
+        status: 200,
+      });
     }
   } catch (error) {
     return new Response("Failed to find recipe", { status: 500 });
