8fb8f37a43f97c4687be6e6a263ef1d758c309ce
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-03-02 13:55:43 +0100

    MyRecipesList desktop styling

--- components/MyRecipesList.jsx
+++ components/MyRecipesList.jsx
@@ -201,42 +201,35 @@ const myRecipes = [
 
 const MyRecipesList = () => {
   return (
-    <div className="flex flex-col gap-4 tablet:gap-10">
+    <div className="flex flex-col gap-4 tablet:gap-10 desktop:gap-12">
       {myRecipes.map((recipe) => (
         <div
-          className="rounded-lg bg-input-primary px-3.5 py-2.5 tablet:px-6 tablet:py-7"
+          className="rounded-lg bg-input-primary px-3.5 py-2.5 tablet:px-6 tablet:py-7 desktop:p-10"
           key={recipe.title}
         >
-          <div className="relative flex gap-4 tablet:gap-6">
+          <div className="relative flex gap-4 tablet:gap-6 desktop:gap-14">
             <Image
               src={recipe.thumb}
               width={124}
               height={124}
               alt="img"
-              className="h-auto rounded-lg tablet:w-[228px]"
+              className="h-auto rounded-lg tablet:w-[228px] desktop:w-[318px]"
             />
-            <div className="absolute right-0 top-0 rounded bg-primary-color p-1.5 tablet:p-2">
+            <div className="absolute right-0 top-0 rounded bg-primary-color p-1.5 tablet:p-2 desktop:p-2.5">
               <FaRegTrashAlt
-                className="text-input-primary tablet:h-5 tablet:w-5"
+                className="text-input-primary tablet:h-5 tablet:w-5 desktop:h-6 desktop:w-6"
                 size="14px"
               />
             </div>
 
             <div className="relative">
-              <div className="max-w-[163px] tablet:max-w-[404px]">
+              <div className="max-w-[163px] tablet:max-w-[404px] desktop:max-w-[806px]">
                 <h4 className="max-w-[143px] overflow-hidden text-ellipsis whitespace-nowrap pt-1.5 text-sm font-medium tablet:max-w-none tablet:pt-0 tablet:text-2xl">
                   {recipe.title}
                 </h4>
-                <p className="mt-3.5 h-10 overflow-hidden text-[8px] leading-[10px] tablet:mt-7 tablet:h-32 tablet:text-sm tablet:leading-5">
-                  <span className="line-clamp-4 tablet:line-clamp-5">
-                    {recipe.description} Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Sint molestiae ea nobis ad, odio in eum
-                    sunt voluptatum! Enim officiis cumque ex quibusdam tenetur
-                    sint veritatis, dicta iure voluptatibus vitae. Lorem ipsum
-                    dolor, sit amet consectetur adipisicing elit. Mollitia quia
-                    possimus, consectetur provident obcaecati pariatur. Impedit
-                    alias, temporibus quaerat doloremque facilis mollitia optio
-                    non id commodi accusamus repellat molestiae nulla.
+                <p className="mt-3.5 h-10 overflow-hidden text-[8px] leading-[10px] tablet:mt-7 tablet:h-32 tablet:text-sm tablet:leading-5 desktop:mt-10 desktop:h-40 desktop:text-lg desktop:leading-6">
+                  <span className="line-clamp-4 tablet:line-clamp-5 desktop:line-clamp-6">
+                    {recipe.description}
                   </span>
                 </p>
               </div>
@@ -244,7 +237,7 @@ const MyRecipesList = () => {
                 20 min
               </span>
             </div>
-            <button className="absolute bottom-0 right-0 rounded-lg rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] bg-primary-color px-4 py-1.5 text-[10px] text-white hover:bg-secondary-light-color focus:outline-none focus:ring-4 focus:ring-secondary-light-color tablet:px-8 tablet:py-3 tablet:text-sm">
+            <button className="absolute bottom-0 right-0 rounded-lg rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] bg-primary-color px-4 py-1.5 text-[10px] text-white hover:bg-secondary-light-color focus:outline-none focus:ring-4 focus:ring-secondary-light-color tablet:px-8 tablet:py-3 tablet:text-sm desktop:px-11 desktop:py-4 desktop:text-base">
               See recipe
             </button>
           </div>
