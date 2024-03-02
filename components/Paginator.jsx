7fe295a4d5896ffd4759bbce8ea21dfa4a96d579
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-03-02 14:31:33 +0100

    added Paginator styling

--- components/Paginator.jsx
+++ components/Paginator.jsx
@@ -1,7 +1,24 @@
+import { Pagination, Stack } from "@mui/material";
 import React from "react";
 
 const Paginator = () => {
-  return <div>Paginator</div>;
+  return (
+    <>
+      <div className="mx-auto mt-10 max-w-max rounded-[26px] bg-input-primary px-7 py-4 shadow-md tablet:mt-12">
+        <Stack spacing={2}>
+          <Pagination
+            count={10}
+            boundaryCount={0}
+            sx={{
+              "& .MuiPaginationItem-root.Mui-selected": {
+                backgroundColor: "#EBF3D4",
+              },
+            }}
+          />
+        </Stack>
+      </div>
+    </>
+  );
 };
 
 export default Paginator;
