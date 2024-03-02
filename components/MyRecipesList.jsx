cbb1de796325c439a0442baeaf7e01c5e837eb3b
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-03-02 13:45:53 +0100

    MyRecipesList mobile styling

--- components/MyRecipesList.jsx
+++ components/MyRecipesList.jsx
@@ -201,44 +201,56 @@ const myRecipes = [
 
 const MyRecipesList = () => {
   return (
-    <>
+    <div className="flex flex-col gap-4 tablet:gap-10">
       {myRecipes.map((recipe) => (
         <div
-          className="rounded-lg bg-input-primary px-3.5 py-2.5"
+          className="rounded-lg bg-input-primary px-3.5 py-2.5 tablet:px-6 tablet:py-7"
           key={recipe.title}
         >
-          <div className="relative flex gap-4">
+          <div className="relative flex gap-4 tablet:gap-6">
             <Image
               src={recipe.thumb}
               width={124}
               height={124}
               alt="img"
-              className="rounded-lg"
+              className="h-auto rounded-lg tablet:w-[228px]"
             />
-            <div className="absolute right-0 top-0 rounded bg-primary-color p-1.5">
-              <FaRegTrashAlt className="text-input-primary" size="14px" />
+            <div className="absolute right-0 top-0 rounded bg-primary-color p-1.5 tablet:p-2">
+              <FaRegTrashAlt
+                className="text-input-primary tablet:h-5 tablet:w-5"
+                size="14px"
+              />
             </div>
 
             <div className="relative">
-              <div className="max-w-[163px]">
-                <h4 className="max-w-[143px] overflow-hidden text-ellipsis whitespace-nowrap pt-1.5 text-sm font-medium">
-                  {recipe.title}ll
+              <div className="max-w-[163px] tablet:max-w-[404px]">
+                <h4 className="max-w-[143px] overflow-hidden text-ellipsis whitespace-nowrap pt-1.5 text-sm font-medium tablet:max-w-none tablet:pt-0 tablet:text-2xl">
+                  {recipe.title}
                 </h4>
-                <p className="mt-3.5 h-10 overflow-hidden text-[8px] leading-[10px]">
-                  <span className="line-clamp-4">{recipe.description}</span>
+                <p className="mt-3.5 h-10 overflow-hidden text-[8px] leading-[10px] tablet:mt-7 tablet:h-32 tablet:text-sm tablet:leading-5">
+                  <span className="line-clamp-4 tablet:line-clamp-5">
+                    {recipe.description} Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Sint molestiae ea nobis ad, odio in eum
+                    sunt voluptatum! Enim officiis cumque ex quibusdam tenetur
+                    sint veritatis, dicta iure voluptatibus vitae. Lorem ipsum
+                    dolor, sit amet consectetur adipisicing elit. Mollitia quia
+                    possimus, consectetur provident obcaecati pariatur. Impedit
+                    alias, temporibus quaerat doloremque facilis mollitia optio
+                    non id commodi accusamus repellat molestiae nulla.
+                  </span>
                 </p>
               </div>
-              <span className="absolute bottom-0 left-0 text-[10px]">
+              <span className="absolute bottom-0 left-0 text-[10px] tablet:text-sm">
                 20 min
               </span>
             </div>
-            <button className="absolute bottom-0 right-0 rounded-lg rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] bg-primary-color px-4 py-1.5 text-[10px] text-white hover:bg-secondary-light-color focus:outline-none focus:ring-4 focus:ring-secondary-light-color tablet:px-14">
+            <button className="absolute bottom-0 right-0 rounded-lg rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] bg-primary-color px-4 py-1.5 text-[10px] text-white hover:bg-secondary-light-color focus:outline-none focus:ring-4 focus:ring-secondary-light-color tablet:px-8 tablet:py-3 tablet:text-sm">
               See recipe
             </button>
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
