85332594cb4b66f39c159707005308f7d3e25b2c
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-03-09 10:24:54 +0100

    added mongoDB adapter

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers";
 import bcrypt from "bcrypt";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@config/mongodbAdapter";
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+
   providers: [],
 };
 
