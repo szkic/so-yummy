21d59038425bb0cc745eeb6d7e28b8a3c5dcbc42
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-03-20 17:23:52 +0100

    sort ingred. by title, prevent adding same ing

--- app/api/shopping-list/route.js
+++ app/api/shopping-list/route.js
@@ -8,9 +8,10 @@ export const POST = async (req, res) => {
   console.log(data);
 
   console.log("user", data.user);
-  // UWAGA - w przesyłanym obiekcie data powinna być flaga czy usuwam czy dodaje???
-  // { id: "640c2dd963a319ea671e373a", measure: "1", action: "add" }
-  // { id: "640c2dd963a319ea671e373a", measure: "1", action: "remove"}
+
+  if (!data.user) {
+    return new Response("User is required", { status: 400 });
+  }
 
   try {
     await connectToDB();
@@ -23,7 +24,12 @@ export const POST = async (req, res) => {
     const filter = { email: data.user };
     const update = {};
 
-    if (data.action === "add") {
+    // check if ingredient is already in the list
+    const isIngredientInList = user.shoppingList.find(
+      (item) => item._id.toString() === data.id,
+    );
+
+    if (data.action === "add" && !isIngredientInList) {
       update.$push = { shoppingList: { _id: data.id, measure: data.measure } };
     }
 
@@ -33,6 +39,32 @@ export const POST = async (req, res) => {
 
     await User.findOneAndUpdate(filter, update, { new: true });
 
+    // add measure to ingredients
+    const ingredientWithMeasure = ingredients.map((ingredient) => {
+      const measure = user.shoppingList.find(
+        (item) => item._id.toString() === ingredient._id.toString(),
+      );
+      return { ...ingredient.toObject(), measure: measure.measure };
+    });
+
+    // sort by title
+    ingredientWithMeasure.sort((a, b) => {
+      const titleA = a.ttl.toLowerCase();
+      const titleB = b.ttl.toLowerCase();
+
+      if (titleA < titleB) {
+        return -1;
+      }
+
+      if (titleA > titleB) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    // console.log("ingredientWithMeasure", ingredientWithMeasure);
+
     // 1. przesłać ID i measure jako obiekt
     // 2. zapisać go do usera
     // 3. przemapować się po userze -> zwrócić obiekt z ingredients i dodać do niego measure
@@ -40,7 +72,7 @@ export const POST = async (req, res) => {
 
     // 5. dodać możliwość usuwania z listy zakupów po przesłanym ID
 
-    return new Response(JSON.stringify(ingredients), { status: 200 });
+    return new Response(JSON.stringify(ingredientWithMeasure), { status: 200 });
   } catch (error) {
     console.log("error", error);
     return new Response("Failed to retrieve shopping list", { status: 500 });
