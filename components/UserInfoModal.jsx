949b14f72e21307a8a177f8dfe76512c254e5b3c
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-03-24 12:13:07 +0100

    change image avatar source

--- components/UserInfoModal.jsx
+++ components/UserInfoModal.jsx
@@ -19,6 +19,8 @@ const UserInfoModal = ({ handleClose, handlePopover }) => {
 
   const user = session.user.email;
 
+  console.log("session", session);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -48,40 +50,13 @@ const UserInfoModal = ({ handleClose, handlePopover }) => {
     <form className="flex flex-col items-center" onSubmit={handleSubmit}>
       <label htmlFor="imageInput">
         <div className="relative mb-[54px] cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="88"
-            height="88"
-            viewBox="0 0 88 88"
-            fill="none"
-          >
-            <circle cx="44" cy="44" r="44" fill="#D9D9D9" />
-          </svg>
-
-          <div className="absolute left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 transform">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="40"
-              height="40"
-              viewBox="0 0 40 40"
-              fill="none"
-            >
-              <path
-                d="M33.3327 35V31.6667C33.3327 29.8986 32.6303 28.2029 31.3801 26.9526C30.1298 25.7024 28.4341 25 26.666 25H13.3327C11.5646 25 9.86888 25.7024 8.61864 26.9526C7.36839 28.2029 6.66602 29.8986 6.66602 31.6667V35"
-                stroke="#C4C4C4"
-                strokeWidth="3"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M20.0006 18.3333C23.6825 18.3333 26.6673 15.3486 26.6673 11.6667C26.6673 7.98477 23.6825 5 20.0006 5C16.3188 5 13.334 7.98477 13.334 11.6667C13.334 15.3486 16.3188 18.3333 20.0006 18.3333Z"
-                stroke="#C4C4C4"
-                strokeWidth="3"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </div>
+          <Image
+            src={session.user.image}
+            width={88}
+            height={88}
+            alt="leaves"
+            className="rounded-full"
+          />
 
           <Image
             src="/../assets/images/plus.svg"
