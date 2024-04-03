3bd8e54f0abf0c2da5718573a7837ff2c95af782
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-04-03 11:29:34 +0200

    add / remove favorites button & toast handle

--- components/RecipePageHero.jsx
+++ components/RecipePageHero.jsx
@@ -4,9 +4,27 @@ import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { ToastContainer, toast } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
+import { fetchFavorites } from "@utils/fetchers";
+import { useEffect, useState } from "react";
+import { set } from "mongoose";
 
 const RecipePageHero = ({ title, description, time, id }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
   const { data: session } = useSession();
+
+  const favorite = useQuery({
+    queryKey: ["favorites"],
+    queryFn: () => fetchFavorites(session.user.email),
+    refetchOnMount: "always",
+  });
+
+  useEffect(() => {
+    const checkIfFavoriteData = favorite.data?.find(
+      (recipe) => recipe._id === id,
+    );
+    setIsFavorite(checkIfFavoriteData !== undefined);
+  }, [favorite.data, id]);
 
   const handleAddToFavorites = async (e) => {
     e.preventDefault();
@@ -17,6 +35,24 @@ const RecipePageHero = ({ title, description, time, id }) => {
       });
 
       toast.success("Recipe added to favorites");
+      setIsFavorite(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRemoveFromFavorites = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.delete("/api/favorite", {
+        data: {
+          user: session.user.email,
+          id,
+        },
+      });
+
+      toast.success("Recipe removed from favorites");
+      setIsFavorite(false);
     } catch (error) {
       console.log(error);
     }
@@ -38,19 +74,28 @@ const RecipePageHero = ({ title, description, time, id }) => {
           <p className="mt-4 text-center text-xs leading-4 tablet:mt-6 tablet:text-lg tablet:leading-6">
             {description}
           </p>
-          <button
-            className="mt-6 rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] border-2 border-primary-color px-5 py-2.5 text-[10px] tablet:rounded-bl-[70px] tablet:rounded-br-[30px] tablet:rounded-tl-[30px] tablet:rounded-tr-[70px] tablet:px-11 tablet:py-5 tablet:text-base"
-            aria-label="Add to favorite"
-            onClick={handleAddToFavorites}
-          >
-            Add to favorite recipes
-          </button>
+          {!isFavorite ? (
+            <button
+              className="mt-6 rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] border-2 border-primary-color px-5 py-2.5 text-[10px] tablet:rounded-bl-[70px] tablet:rounded-br-[30px] tablet:rounded-tl-[30px] tablet:rounded-tr-[70px] tablet:px-11 tablet:py-5 tablet:text-base"
+              aria-label="Add to favorite"
+              onClick={handleAddToFavorites}
+            >
+              Add to favorite recipes
+            </button>
+          ) : (
+            <button
+              className="mt-6 rounded-bl-[35px] rounded-br-[15px] rounded-tl-[15px] rounded-tr-[35px] border-2 border-primary-color px-5 py-2.5 text-[10px] tablet:rounded-bl-[70px] tablet:rounded-br-[30px] tablet:rounded-tl-[30px] tablet:rounded-tr-[70px] tablet:px-11 tablet:py-5 tablet:text-base"
+              aria-label="Add to favorite"
+              onClick={handleRemoveFromFavorites}
+            >
+              Remove from favorite recipes
+            </button>
+          )}
           <p className="mt-10 flex items-center justify-center gap-2 text-[10px] font-medium tablet:text-sm">
             <AccessTimeIcon fontSize="small" /> {time} min
           </p>
         </div>
       </div>
-      <ToastContainer />
     </>
   );
 };
