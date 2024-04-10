8637de3e64c4024a2e707498b7b5e6dc77ad91e0
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-04-10 17:34:25 +0200

    added toast on recipe remove from favorites

--- components/FavoritesList.jsx
+++ components/FavoritesList.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import RecipeItem from "./RecipeItem";
 import { useSession } from "next-auth/react";
 import { useMutation } from "@tanstack/react-query";
+import { ToastContainer, toast } from "react-toastify";
 
 const FavoritesList = ({ data, refetch }) => {
   const { data: session } = useSession();
@@ -19,22 +20,28 @@ const FavoritesList = ({ data, refetch }) => {
   const handleDeleteFromFavorites = async (id) => {
     try {
       mutation.mutateAsync(id);
+
+      toast.success("Recipe successfully removed");
     } catch (error) {
       console.error("Failed to delete item from favorites:", error);
     }
   };
 
   return (
-    <div className="flex flex-col gap-4 tablet:gap-10 desktop:gap-12">
-      {data.map((recipe) => (
-        <RecipeItem
-          key={recipe._id}
-          recipe={recipe}
-          theme="favorites"
-          onDelete={handleDeleteFromFavorites}
-        />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-4 tablet:gap-10 desktop:gap-12">
+        {data.map((recipe) => (
+          <RecipeItem
+            key={recipe._id}
+            recipe={recipe}
+            theme="favorites"
+            onDelete={handleDeleteFromFavorites}
+          />
+        ))}
+      </div>
+
+      <ToastContainer />
+    </>
   );
 };
 
