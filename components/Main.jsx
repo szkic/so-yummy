d36897fe9e8d47b321381d36189d87fdb5a62e9d
bartosz.sz <bartosz.szkiladz@gmail.com>
2024-04-24 18:41:28 +0200

    fix bowl img path

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,17 +2,16 @@ import ChooseYourBreakfast from "@components/ChooseYourBreakfast";
 import PreviewCategories from "@components/PreviewCategories";
 import Search from "@components/Search";
 import Image from "next/image";
-// import bowl from "../public/assets/images/bowl.png";
+import BOWL from "../public/assets/images/bowl.png";
 
 const Main = () => {
   return (
     <>
       <section className="relative flex flex-col flex-wrap items-center pb-20 tablet:flex-row tablet:pb-48 desktop:px-14 desktop:pb-64">
         <Image
-          src="/../assets/images/bowl.png"
+          src={BOWL}
           width={320}
           height={296}
-          // src={bowl}
           alt="bowl"
           className="absolute z-[-1] mt-[210px] h-auto w-auto tablet:right-[-50px] tablet:top-[-170px] tablet:w-[378px] desktop:right-[150px] desktop:top-[-200px] desktop:w-[578px]"
         />
